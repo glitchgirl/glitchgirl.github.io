18b8f69ac4131609e89b1bc2e04a63ca880c2df8
glitchgirl <morganchasesmith@gmail.com>
2019-06-17 21:05:29 -0400

    vue lesson 2

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ var app = new Vue({
     el:'#app',
     data() {
         return {
-            description: 'so we gonna put a picture here'
+            description: 'so we gonna put a picture here',
+            githubLink:"https://github.com/glitchgirl"
         }
     }
 })
