7eb5214d2bab3c0fadd65f5fcc3bba181b621620
glitchgirl <morganchasesmith@gmail.com>
2019-06-17 21:31:33 -0400

    add picture

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ var app = new Vue({
     el:'#app',
     data() {
         return {
-            description: 'so we gonna put a picture here',
-            githubLink:"https://github.com/glitchgirl"
+            description: 'so we gonna put a picture here we gonna write a pretty thing about me as a human  then we summarize resume then direct Portfolio or whatever',
+            githubLink:"https://github.com/glitchgirl",
+            instaLink:"https://www.instagram.com/morganchasemakes/?hl=en",
+            image:"/me.jpg"
         }
     }
 })
