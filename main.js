b58d003a769c4f619bb0539b8a37e264bfaa1459
glitchgirl <morganchasesmith@gmail.com>
2019-06-23 21:03:17 -0400

    vue lesson 3

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 var app = new Vue({
     el:'#app',
-    data() {
-        return {
+    data: {
+        return: {
             description: 'so we gonna put a picture here we gonna write a pretty thing about me as a human  then we summarize resume then direct Portfolio or whatever',
             githubLink:"https://github.com/glitchgirl",
             instaLink:"https://www.instagram.com/morganchasemakes/?hl=en",
-            image:"/me.jpg"
+            image:"/me.jpg",
+            date: new Date().getDate(), 
+            dateText:""
+        },
+        methods: {
+            checkToday: function(){
+               return 
+               {
+                   if(date % 2 !=0){
+                        dateText: "Today's date is Even"
+                    } else
+                        dateText: "Today's date is odd"
+                };
+            }
+          
         }
     }
-})
+});
+
+ 
