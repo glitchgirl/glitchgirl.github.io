c21a66090100c4d3ff5ccc768baf6bb883a8e747
glitchgirl <morganchasesmith@gmail.com>
2019-06-23 22:01:09 -0400

    added picture + updated contact page

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ var app = new Vue({
             description: 'so we gonna put a picture here we gonna write a pretty thing about me as a human  then we summarize resume then direct Portfolio or whatever',
             githubLink:"https://github.com/glitchgirl",
             instaLink:"https://www.instagram.com/morganchasemakes/?hl=en",
-            image:"/me.jpg",
+            linkedLink:"https://www.linkedin.com/in/morgan-smith-bb4258139/",
+            imageMe:"/me.jpg",
+            imgageConBack:"/background.jpg",
             date: new Date().getDate(), 
             dateText:""
         }
